c0d312621c192d83dce7bf8fb376c5fa672dde44
Kiou <pinelli.luc@hotmail.fr>
2015-10-09 22:37:26 +0200

    Correction de bug

--- index.php
+++ index.php
@@ -18,7 +18,6 @@
 <body>
 
 	<!-- Contenu -->
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur minus libero quis culpa nostrum modi ab facilis doloribus sequi aspernatur, unde voluptas totam. Atque architecto voluptate, eaque aliquid possimus a neque. Velit dolore, aspernatur ullam ratione a libero possimus sed autem, ab sunt natus doloremque, blanditiis unde dolorem tenetur aliquam asperiores. Labore possimus molestias, ad nemo harum quos pariatur omnis, nisi atque nam, sit assumenda quod eveniet quasi esse. Accusantium sequi minus ullam excepturi itaque dolore natus doloribus omnis perferendis ipsum nemo, deleniti! Impedit iste cum eveniet, sapiente voluptas quis repellendus praesentium repellat vel minus alias quaerat, quam, odio voluptatum. Quod doloremque aut eveniet provident nisi officia, aliquid beatae unde eaque ipsum architecto iusto, aperiam, soluta dicta corrupti distinctio! Alias!</p>
 
 	<script type="text/javascript" src="<?= BASEFRONT ?>js/jquery/jquery.js"></script>
 	<script type="text/javascript" src="<?= BASEFRONT ?>js/app.js"></script>	
