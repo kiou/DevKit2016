0f79c6ace2162bd31b02c0499c78f482f1b453fb
Kiou <pinelli.luc@hotmail.fr>
2015-10-09 22:33:58 +0200

    Mise à jour Jquery ui et ajout datepicher

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 	<title>DevKit 2016</title>
 	<link rel="icon" type="image/png" href="<?= BASEFRONT ?>img/layout/favicon.png">
 	<link href="<?= BASEFRONT ?>css/app.css" rel="stylesheet" type="text/css">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
 	<!--[if lt IE 9]>
 		<script src="<?= BASEFRONT ?>js/html5.js"></script>
 	<![endif]-->
@@ -19,6 +18,7 @@
 <body>
 
 	<!-- Contenu -->
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur minus libero quis culpa nostrum modi ab facilis doloribus sequi aspernatur, unde voluptas totam. Atque architecto voluptate, eaque aliquid possimus a neque. Velit dolore, aspernatur ullam ratione a libero possimus sed autem, ab sunt natus doloremque, blanditiis unde dolorem tenetur aliquam asperiores. Labore possimus molestias, ad nemo harum quos pariatur omnis, nisi atque nam, sit assumenda quod eveniet quasi esse. Accusantium sequi minus ullam excepturi itaque dolore natus doloribus omnis perferendis ipsum nemo, deleniti! Impedit iste cum eveniet, sapiente voluptas quis repellendus praesentium repellat vel minus alias quaerat, quam, odio voluptatum. Quod doloremque aut eveniet provident nisi officia, aliquid beatae unde eaque ipsum architecto iusto, aperiam, soluta dicta corrupti distinctio! Alias!</p>
 
 	<script type="text/javascript" src="<?= BASEFRONT ?>js/jquery/jquery.js"></script>
 	<script type="text/javascript" src="<?= BASEFRONT ?>js/app.js"></script>	
